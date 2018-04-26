cfc617566b5ed7ded6104c6c97af51e0659696f4
KomaWasTaken <valentin.gaillard-21@outlook.fr>
2018-04-26 11:23:58 +0200

    Navigation fonctionnelle mais retourne plus bas que

--- index.php
+++ index.php
@@ -9,27 +9,26 @@
 		<h1>C'est très l'exploration</h1>
 
 		<div>
+			<div>
+				<a href="index.php">Retour à l'index</a><br> <!-- On l'affiche -->
+			</div>
 			<?php foreach($dirs as $dir){?> <!--Parcourt le tableau-->
 				<?php if (is_dir($base_url.$dir)){?> <!-- Si $base_url.$dir est un dossier -->
-					<?php if ($dir == "..") {?> <!-- Si $dir est un parent -->
-						<div>
-							<a href="index.php">Retour à l'index</a><br> <!-- On l'affiche -->
-						</div>
-						<div>
-							<a href="index.php?dossier=<?php dirname('.'.$base_url)?>">Retour au parent</a><br> <!-- On l'affiche -->
-						</div>
+					
+					<?php if ($dir == "../") {?> <!-- Si $dir est un parent -->
 					<?php } else {?> <!-- Si $dir n'est PAS un parent -->
 						<div>
 							<?php if (isset($_GET['dossier'])) {?> <!-- Si  le dossier n'est pas null -->
-								<a href="index.php?dossier=<?php echo $_GET['dossier'] ?><?php echo $dir; ?>/"><?php echo $dir;?></a><br> <!-- l'url prends la valeur du dossier choisi et affiche ce qu'il y a dedans -->
+								<a href="index.php?dossier=<?php echo $_GET['dossier'] ;?><?php echo $dir; ?>/"><?php echo $dir;?></a><br /> <!-- l'url prends la valeur du dossier choisi et affiche ce qu'il y a dedans -->
 							<?php } else {?>
-								<a href="index.php?dossier=<?php echo $dir ?>/"><?php echo $dir; ?></a><br> <!-- Il affiche  -->
+								<a href="index.php?dossier=<?php echo $dir; ?>/"><?php echo $dir; ?></a><br /> <!-- Il affiche  -->
 							<?php }?>
 						</div>
 					<?php }?>
 				<?php } else {?>
+				
 					<div>
-						<p><?php echo $dir; ?></p>
+						<?php echo $dir; ?>
 					</div>
 				<?php }?>
                 <?php }?>
