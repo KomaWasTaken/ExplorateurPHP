006514447aed6cf4fca1d4cd7cfc8814edd1945f
Jim Pronesti <drak3r21@hotmail.com>
2018-04-27 14:04:36 +0200

    Intégration du footer

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> 
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 		<link rel="stylesheet" href="style.css">
 		<title>C'est très l'exploration</title>
 	</head>
 	<body class="d-flex jc-center align-items-center flex-column">
 		<h1 class="text-align-center color-white">C'est très l'exploration</h1>
 
-		<div class="w60pct h80pct d-flex flex-row flex-wrap p15px fs-18 montserrat word-break">
+		<div class="h100vh w70pct d-flex flex-row flex-wrap p15px fs-18 montserrat word-break">
 			
 				<a class="p15px w20pct box h30pct borderBox mt5px" href="index.php"><div class="d-flex text-align-center jc-center align-items-center h100pct">Retour à l'index</div></a><br> <!-- On l'affiche -->
 			
@@ -23,7 +24,6 @@
 						<?php }?> 
 						
 					<?php } else {?> <!-- Si $dir n'est PAS un parent -->
-						
 							<?php if (isset($_GET['dossier'])) {?> <!-- Si  le dossier n'est pas null -->
 								
 								<a class="flex-row p15px w20pct text-align-center box h30pct borderBox mt5px" href="index.php?dossier=<?php echo $_GET['dossier'] ?><?php echo $dir; ?>/"><div class=""><i class="far fa-folder-open d-flex fs-60 mt5px"></i><?php echo $dir;?></div></a><br> <!-- l'url prends la valeur du dossier choisi et affiche ce qu'il y a dedans -->
@@ -31,16 +31,32 @@
 								
 								<a class="p15px w20pct text-align-center box h30pct borderBox mt5px" href="index.php?dossier=<?php echo $dir ?>/"><div class><i class="far fa-folder-open d-flex fs-60 mt5px"></i><?php echo $dir; ?></div></a><br> <!-- Il affiche  -->
 							<?php }?>
-						
 					<?php }?>
 				<?php } else {?>
 					<a class="p15px w20pct text-align-center box h30pct borderBox mt5px" href="#"><div>
 						<p><i class="far fa-file d-flex fs-60"></i> <p class="pt5px"><?php echo $dir; ?></p></p>
 					</div></a>
-					
-					
+				
 				<?php }?>
-                <?php }?>
+				<?php }?>
+				
+
 		</div>
+<footer class="w100pct bg-black d-flex jc-center">
+	<div class="d-flex color-gray roboto jc-space-between h100 w80pct">
+		<div class="d-flex align-items-center">
+			<p>2018 © Copyright - Monica.B, Alpha.B, Valentin.G, Jim.P, Ludovic.R</p>
+		</div>
+		<div class="d-flex align-items-center">
+			<i class="pl15px fab fa-facebook-f fa-2x"></i>
+			<i class="pl15px fab fa-twitter fa-2x"></i>
+			<i class="pl15px fab fa-linkedin-in fa-2x"></i>
+			<i class="pl15px fab fa-google-plus-g fa-2x"></i>
+			<i class="pl15px fab fa-instagram fa-2x"></i>
+			<i class="pl15px far fa-envelope fa-2x"></i>
+		</div>
+	</div>
+</footer>
+
 </body>
 </html>
