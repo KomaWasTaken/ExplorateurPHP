9a5ab34d477089c798cba2b955eb23ab9ac2dcbd
Jim Pronesti <drak3r21@hotmail.com>
2018-04-27 09:11:52 +0200

    Mise en place du CSS

--- index.php
+++ index.php
@@ -3,33 +3,41 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+		<link rel="stylesheet" href="style.css">
 		<title>C'est très l'exploration</title>
 	</head>
-	<body>
-		<h1>C'est très l'exploration</h1>
+	<body class="d-flex jc-center align-items-center flex-column">
+		<h1 class="text-align-center">C'est très l'exploration</h1>
 
-		<div>
-			<div>
+		<div class="w60pct h80pct d-flex flex-row flex-wrap jc-space-around p15px ">
+			<div class="p15px w20pct text-align-center box h30pct borderBox mt2px">
 				<a href="index.php">Retour à l'index</a><br> <!-- On l'affiche -->
 			</div>
 			<?php foreach($dirs as $dir){?> <!--Parcourt le tableau-->
 				<?php if (is_dir($base_url.$dir)){?> <!-- Si $base_url.$dir est un dossier -->
-					
-					<?php if ($dir == "../") {?> <!-- Si $dir est un parent -->
+					<?php if ($dir == "..") {?> <!-- Si $dir est un parent -->
+						<?php if (isset($_GET['dossier'])){?>
+							<a class="p15px w20pct text-align-center box h30pct borderBox mt2px" href="index.php?dossier=<?php echo $_GET['dossier'] ?><?php echo $dir; ?>/">Retour</a>
+						<?php }?> 
+						
 					<?php } else {?> <!-- Si $dir n'est PAS un parent -->
-						<div>
+						
 							<?php if (isset($_GET['dossier'])) {?> <!-- Si  le dossier n'est pas null -->
-								<a href="index.php?dossier=<?php echo $_GET['dossier'] ;?><?php echo $dir; ?>/"><?php echo $dir;?></a><br /> <!-- l'url prends la valeur du dossier choisi et affiche ce qu'il y a dedans -->
+								
+								<a class="flex-row p15px w20pct text-align-center box h30pct borderBox mt2px" href="index.php?dossier=<?php echo $_GET['dossier'] ?><?php echo $dir; ?>/"><div><i class="far fa-folder-open d-flex fs-60"></i><?php echo $dir;?></div></a><br> <!-- l'url prends la valeur du dossier choisi et affiche ce qu'il y a dedans -->
 							<?php } else {?>
-								<a href="index.php?dossier=<?php echo $dir; ?>/"><?php echo $dir; ?></a><br /> <!-- Il affiche  -->
+								
+								<a class="p15px w20pct text-align-center box h30pct borderBox mt2px" href="index.php?dossier=<?php echo $dir ?>/"><div><i class="far fa-folder-open d-flex fs-60"></i><?php echo $dir; ?></div></a><br> <!-- Il affiche  -->
 							<?php }?>
-						</div>
+						
 					<?php }?>
 				<?php } else {?>
-				
-					<div>
-						<?php echo $dir; ?>
-					</div>
+					<a class="p15px w20pct text-align-center box h30pct borderBox mt2px" href="#"><div>
+						<p><i class="far fa-file d-flex fs-60"></i><?php echo $dir; ?></p>
+					</div></a>
+					
+					
 				<?php }?>
                 <?php }?>
 		</div>
