0d696a92ac8ecac845728540ce35b5f986390dcd
Jim Pronesti <drak3r21@hotmail.com>
2018-04-27 11:30:42 +0200

    Ajout transitions, modif structure, correction
    Correction des textes trop longs avec break-word.
    Transitions hover sur les div
    Ajout de font (MontSerrat)
    Ajout de font icon
    Ajout de couleur de texte
    Changement de la disposition
    
    Fait avec @KomaWasTaken

--- index.php
+++ index.php
@@ -4,37 +4,38 @@
 	<head>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> 
 		<link rel="stylesheet" href="style.css">
 		<title>C'est très l'exploration</title>
 	</head>
 	<body class="d-flex jc-center align-items-center flex-column">
-		<h1 class="text-align-center">C'est très l'exploration</h1>
+		<h1 class="text-align-center color-white">C'est très l'exploration</h1>
 
-		<div class="w60pct h80pct d-flex flex-row flex-wrap jc-space-around p15px ">
-			<div class="p15px w20pct text-align-center box h30pct borderBox mt2px">
-				<a href="index.php">Retour à l'index</a><br> <!-- On l'affiche -->
-			</div>
+		<div class="w60pct h80pct d-flex flex-row flex-wrap p15px fs-18 montserrat word-break">
+			
+				<a class="p15px w20pct box h30pct borderBox mt5px" href="index.php"><div class="d-flex text-align-center jc-center align-items-center h100pct">Retour à l'index</div></a><br> <!-- On l'affiche -->
+			
 			<?php foreach($dirs as $dir){?> <!--Parcourt le tableau-->
 				<?php if (is_dir($base_url.$dir)){?> <!-- Si $base_url.$dir est un dossier -->
 					<?php if ($dir == "..") {?> <!-- Si $dir est un parent -->
 						<?php if (isset($_GET['dossier'])){?>
-							<a class="p15px w20pct text-align-center box h30pct borderBox mt2px" href="index.php?dossier=<?php echo $_GET['dossier'] ?><?php echo $dir; ?>/">Retour</a>
+							<a class="p15px w20pct box h30pct borderBox mt5px" href="index.php?dossier=<?php echo $_GET['dossier'] ?><?php echo $dir; ?>/"> <div class="d-flex text-align-center jc-center align-items-center h100pct">Retour</div></a>
 						<?php }?> 
 						
 					<?php } else {?> <!-- Si $dir n'est PAS un parent -->
 						
 							<?php if (isset($_GET['dossier'])) {?> <!-- Si  le dossier n'est pas null -->
 								
-								<a class="flex-row p15px w20pct text-align-center box h30pct borderBox mt2px" href="index.php?dossier=<?php echo $_GET['dossier'] ?><?php echo $dir; ?>/"><div><i class="far fa-folder-open d-flex fs-60"></i><?php echo $dir;?></div></a><br> <!-- l'url prends la valeur du dossier choisi et affiche ce qu'il y a dedans -->
+								<a class="flex-row p15px w20pct text-align-center box h30pct borderBox mt5px" href="index.php?dossier=<?php echo $_GET['dossier'] ?><?php echo $dir; ?>/"><div class=""><i class="far fa-folder-open d-flex fs-60 mt5px"></i><?php echo $dir;?></div></a><br> <!-- l'url prends la valeur du dossier choisi et affiche ce qu'il y a dedans -->
 							<?php } else {?>
 								
-								<a class="p15px w20pct text-align-center box h30pct borderBox mt2px" href="index.php?dossier=<?php echo $dir ?>/"><div><i class="far fa-folder-open d-flex fs-60"></i><?php echo $dir; ?></div></a><br> <!-- Il affiche  -->
+								<a class="p15px w20pct text-align-center box h30pct borderBox mt5px" href="index.php?dossier=<?php echo $dir ?>/"><div class><i class="far fa-folder-open d-flex fs-60 mt5px"></i><?php echo $dir; ?></div></a><br> <!-- Il affiche  -->
 							<?php }?>
 						
 					<?php }?>
 				<?php } else {?>
-					<a class="p15px w20pct text-align-center box h30pct borderBox mt2px" href="#"><div>
-						<p><i class="far fa-file d-flex fs-60"></i><?php echo $dir; ?></p>
+					<a class="p15px w20pct text-align-center box h30pct borderBox mt5px" href="#"><div>
+						<p><i class="far fa-file d-flex fs-60"></i> <p class="pt5px"><?php echo $dir; ?></p></p>
 					</div></a>
 					
 					
